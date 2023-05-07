7ce6affe317e1b1ba3bfa9441a8fa512d1261918
poudyalamit <poudyalamit20@gmail.com>
2023-05-07 22:37:20 +0545

    Added tailwind css and some config files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import Graph from './Components/Graph';
 
 function App() {
   return (
     <div className="App">
+      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+        <h1 className='text-4xl py-8 mb-10 bg-slate-800 text-white rounded'>Xpense</h1>
+
+        {/* grid columns */}
+
+        <div className="grid md:grid-cols-2 gap-4" >
+          {/* chart */}
+          <Graph/>
+          {/* form */}
+        </div>
+      </div>
     </div>
   );
 }
