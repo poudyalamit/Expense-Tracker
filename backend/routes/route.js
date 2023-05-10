fe6cc65459d1fb8fca7ad510833e6649843cad01
poudyalamit <poudyalamit20@gmail.com>
2023-05-10 22:40:15 +0545

    added controllers for routes

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,7 +1,8 @@
 const routes = require('express').Router();
+const controller=require('../Conroller/controller');
 
-
-routes.route('/api/categories').get((req, res) => res.send("Get Req From Categories"))
+routes.route('/api/categories')
+    .get(controller.create_Categories);
 
 
 module.exports = routes;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-const express=require('express');
+ const express=require('express');
 const app =express();
 const cors=require('cors');
+require('dotenv').config({path:'./config.env'});
 
-const port= 8080;
-
+const port= process.env.PORT || 5000;
 //use middlewares
 app.use(cors);
 app.use(express.json());
@@ -14,5 +14,5 @@ app.use(require('./routes/route'));
 
 
 app.listen(port,()=>{
-    console.log(`Xpense backend server running on port:http://localhost:${port}`)
+    console.log(`Xpense backend server running on port: http://localhost:${port}`)
 })
